41c541e5a663d325530bff255db329d8e90aca5d
Pitchu-mani <91552961+Pitchu-mani@users.noreply.github.com>
2025-01-14 16:04:19 +0530

    Update eval.cpp

--- Phase2/evaluation/eval.cpp
+++ Phase2/evaluation/eval.cpp
@@ -67,3 +67,82 @@ int main()
      return 0;
         
 }
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <string>
+using namespace std;
+
+class LabTest
+{
+private:
+    string testId;
+    double resultValue;
+
+public:
+    LabTest(string p_TestID, double p_ResultValue)
+        : testId(p_TestID), resultValue(p_ResultValue) {}
+
+    double getResultValue() const
+    {
+        return resultValue;
+    }
+};
+
+int findSum(const vector<LabTest> &input)
+{
+    int sum = 0;
+    for (const auto &test : input)
+    {
+        sum += test.getResultValue();
+    }
+    return sum;
+}
+
+int findMinInFirstHalf(const vector<LabTest> &input)
+{
+    int min = INT_MAX;
+    int n = input.size();
+    for (int i = 0; i < n / 2; ++i)
+    {
+        min = (min < input[i].getResultValue()) ? min : input[i].getResultValue();
+    }
+    return min;
+}
+
+int findMaxInSecondHalf(const vector<LabTest> &input)
+{
+    int max = INT_MIN;
+    int n = input.size();
+    for (int i = n / 2; i < n; ++i)
+    {
+        max = (max > input[i].getResultValue()) ? max : input[i].getResultValue();
+    }
+    return max;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of Lab tests:" << endl;
+    cin >> n;
+
+    vector<LabTest> input;
+    for (int i = 0; i < n; ++i)
+    {
+        string testId;
+        double resultValue;
+        cout << "Enter test ID and result value for test " << i + 1 << ":" << endl;
+        cin >> testId >> resultValue;
+        LabTest test(testId, resultValue);
+        input.push_back(test);
+    }
+
+    int result = findSum(input);
+    cout << "The sum of results is: " << result << endl;
+
+    int resultFirstMin = findMinInFirstHalf(input);
+    cout << "The minimum value in the first half is: " << resultFirstMin << endl;
+
+    int resultSecondMax = findMaxInSecondHalf
+
