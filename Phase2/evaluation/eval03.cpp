a19147b3316bb09d4b3143a6247cff6063f818c8
Pitchu-mani <91552961+Pitchu-mani@users.noreply.github.com>
2025-01-15 10:03:09 +0530

    Update eval03.cpp

--- Phase2/evaluation/eval03.cpp
+++ Phase2/evaluation/eval03.cpp
@@ -1,18 +1,15 @@
-#include<iostream>
-#include<vector>
-#include<climits>
-#include<string>
-#include<thread>
-#include<algorithm>
-#include<unistd.h>
-#include<cstring>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unistd.h>
+#include <cstring>
 
 using namespace std;
 
-class labTest {
+class LabTest {
 private:
-	string testId;
-	double resultValue;
+    string testId;
+    double resultValue;
 
 public:
     LabTest(string p_TestID, double p_ResultValue)
@@ -23,118 +20,88 @@ public:
     }
 };
 
-
-void findSum(const vector<labTest>& labTests, double& sum) {
-    for (auto& test : labtests) {
-        sum += test.getResultValue();
+void findSum(const vector<double>& results, double& sum) {
+    for (const auto& value : results) {
+        sum += value;
     }
 }
 
+void client(const vector<LabTest>& labTests, int write_end_fd1, int read_end_fd2) {
+    int size = labTests.size();
+    write(write_end_fd1, &size, sizeof(size));
 
-void client( vector<labTest>& labTests, int& write_end_fd1, int& read_end_fd2){
-	int size =labtests.size();
-	write(write_end_fd1, &size, 4);
-	int buffer[size];
-	for(int i = 0; i < labtests.size(); i++){
-		buffer[i]=(labTests[i].getResultValue());
-	}
-	write(write_end_fd1, buffer, sizeof(buffer));
-	
-	cout << "client : " << buffer[1] << endl;
-	close(write_end_fd1);
-	sleep(2);
+    vector<double> resultValues;
+    for (const auto& test : labTests) {
+        resultValues.push_back(test.getResultValue());
+    }
+    write(write_end_fd1, resultValues.data(), resultValues.size() * sizeof(double));
 
-	int sum=0;
-	read(read_end_fd2, &sum, sizeof(int));
+    cout << "Client: Sent results to the server." << endl;
+    close(write_end_fd1);
 
-	cout << "total sum : "<< sum << endl;
-	close(read_end_fd2);
+    double sum = 0;
+    read(read_end_fd2, &sum, sizeof(double));
 
-
+    cout << "Total sum received by client: " << sum << endl;
+    close(read_end_fd2);
 }
 
-void server(int& read_end_fd1,int& write_end_fd2){
-	sleep(1);
+void server(int read_end_fd1, int write_end_fd2) {
+    int size;
+    read(read_end_fd1, &size, sizeof(size));
 
-	int size;
+    vector<double> results(size);
+    read(read_end_fd1, results.data(), size * sizeof(double));
 
-	read(read_end_fd1,&size,sizeof(size));
+    cout << "Server: Received results from the client." << endl;
+    close(read_end_fd1);
 
-	int a[size];
+    double sum = 0;
+    findSum(results, sum);
 
-	read(read_end_fd1,
-	a,
-	sizeof(int)*size);
-
-	cout << "server ..." << endl;
-	close(read_end_fd1);
-	int s= 0; 
-	findSum(a,s);
-	write(write_end_fd2, &s, sizeof(s));
-	cout << "sum sent : " <<s<< endl;
-	close(write_end_fd2);
-
+    write(write_end_fd2, &sum, sizeof(sum));
+    cout << "Server: Sent sum back to the client: " << sum << endl;
+    close(write_end_fd2);
 }
 
 int main() {
     int n;
-    cout << "Enter the number of lab tests: " << endl;
+    cout << "Enter the number of lab tests: ";
     cin >> n;
 
     vector<LabTest> labTests;
     for (int i = 1; i <= n; ++i) {
         string testId;
         double resultValue;
-        cout << "Enter test ID and result value for test " << i << ":" << endl;
+        cout << "Enter test ID and result value for test " << i << ": ";
         cin >> testId >> resultValue;
-        LabTest test(testId, resultValue);
-        labTests.push_back(test);
+        labTests.emplace_back(testId, resultValue);
     }
 
     int pipe_fd1[2];
     int pipe_fd2[2];
-    if(pipe(pipe_fd1)==-1){
+    if (pipe(pipe_fd1) == -1 || pipe(pipe_fd2) == -1) {
         perror("pipe");
-        cout << "pipe not created" << endl;
-        return 1;
-    }
-	if(pipe(pipe_fd2)==-1){
-        perror("pipe");
-        cout << "pipe not created" << endl;
+        cout << "Pipe creation failed." << endl;
         return 1;
     }
 
-    int& read_end_fd1 = pipe_fd1[0];
-    int& write_end_fd1 = pipe_fd1[1];
-    int& write_end_fd2 = pipe_fd2[1];
-    int& read_end_fd2 = pipe_fd2[0];
-
-
-
-    pid_t pid = -1;
-
-    {
-		pid = fork();
-        if(0==pid){
-			close(read_end_fd1);
-			client(vec,write_end_fd1,read_end_fd2);
-			return 0;
-		}
+    pid_t pid = fork();
+    if (pid == 0) {
+        // Child process (Client)
+        close(pipe_fd1[0]); // Close unused read end of pipe 1
+        close(pipe_fd2[1]); // Close unused write end of pipe 2
+        client(labTests, pipe_fd1[1], pipe_fd2[0]);
+        return 0;
+    } else if (pid > 0) {
+        // Parent process (Server)
+        close(pipe_fd1[1]); // Close unused write end of pipe 1
+        close(pipe_fd2[0]); // Close unused read end of pipe 2
+        server(pipe_fd1[0], pipe_fd2[1]);
+    } else {
+        perror("fork");
+        return 1;
     }
 
-	{
-		pid = fork();
-		if(0==pid){
-			close(write_end_fd1);
-			server(read_end_fd1,write_end_fd2);
-			return 0;
-		}
-	}
-
-
-	sleep(3);
-
-	return 0;
-
-
+    return 0;
 }
